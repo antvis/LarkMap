bd3bc08243853199d1a8f139306a009ee80ab053
yunji <yunji.me@outlook.com>
2022-07-20 13:10:40 +0800

    fix: 修复 useLayer 动态加载图层情况

--- src/components/LarkMap/hooks/use-layer/index.ts
+++ src/components/LarkMap/hooks/use-layer/index.ts
@@ -1,9 +1,38 @@
+import { useEffect, useState } from 'react';
 import type { Layer } from '../../../../utils';
 import { useLayerManager } from '../use-layer-manager';
 
-export const useLayer = <T extends Layer = Layer>(id: string): T => {
+export const useLayer = <T extends Layer = Layer>(id: string): T | undefined => {
   const layerManager = useLayerManager();
-  const layer = layerManager.getLayer(id);
+  const [layer, setLayer] = useState<T | undefined>();
 
-  return layer as T;
+  useEffect(() => {
+    const onLayerAdd = (_layer: T) => {
+      if (id === _layer.id) {
+        setLayer(_layer);
+      }
+    };
+    const onLayerRemove = (layerId: string) => {
+      if (id === layerId) {
+        setLayer(undefined);
+      }
+    };
+    const _layer = layerManager.getLayer(id);
+
+    if (_layer) {
+      setLayer(_layer as T);
+    } else {
+      layerManager.on('add', onLayerAdd);
+      layerManager.on('remove', onLayerRemove);
+    }
+
+    return () => {
+      if (_layer === undefined) {
+        layerManager.off('add', onLayerAdd);
+        layerManager.off('remove', onLayerRemove);
+      }
+    };
+  }, [id]);
+
+  return layer;
 };
