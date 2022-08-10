651eb39768660e2e44c974bd921f2fe647c15cb6
yxh01132861 <yxh01132861@antgroup.com>
2022-08-10 17:35:48 +0800

    fix: 问题处理

--- src/components/LayerAttribute/HeatmapLayerStyle/constant.ts
+++ src/components/LayerAttribute/HeatmapLayerStyle/constant.ts
@@ -10,9 +10,9 @@ export const DefaultHeatmapLayerStyle: HeatmapLayerStyleAttributeValue = {
     value: [12, 15],
   },
   style: {
-    intensity: 3, //全局热力权重
-    radius: 20, //热力半径，单位像素
-    opacity: 1, //透明度
+    intensity: 3,
+    radius: 20,
+    opacity: 1,
     rampColors: {
       colors: ['#FF4818', '#F7B74A', '#FFF598', '#F27DEB', '#8C1EB2', '#421EB2'], //色带
       positions: [0, 0.2, 0.4, 0.6, 0.8, 1.0],
