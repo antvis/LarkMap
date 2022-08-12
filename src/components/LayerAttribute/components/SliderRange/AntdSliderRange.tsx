9bd07293a2b887baca86a85f44c96970eeaa8d9c
yxh01132861 <yxh01132861@antgroup.com>
2022-08-12 14:04:23 +0800

    fix: 选择填充半径range

--- src/components/LayerAttribute/components/SliderRange/AntdSliderRange.tsx
+++ src/components/LayerAttribute/components/SliderRange/AntdSliderRange.tsx
@@ -1,6 +1,6 @@
 import type { SliderRangeProps } from 'antd/lib/slider';
 import { InputNumber, Slider } from 'antd';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { usePrefixCls } from '@formily/antd/esm/__builtins__/hooks/usePrefixCls';
 import './AntdSliderRange.less';
 
@@ -13,44 +13,55 @@ const defaultConfig = {
 const AntdSliderRange: React.FC<SliderRangeProps> = (props) => {
   const prefixCls = usePrefixCls('formily-slider-range', props);
   const config = { ...defaultConfig, ...props };
+  const [sliderVal, setSliderVal] = useState<[number, number]>(config.value ?? [0, 0]);
+
+  useEffect(() => {
+    props.onChange(sliderVal);
+  }, [sliderVal]);
 
   return (
     <div className={prefixCls}>
-      <Slider range={true} {...config} />
+      <Slider
+        range={true}
+        {...config}
+        onChange={(val) => {
+          setSliderVal(val);
+        }}
+      />
       <div className={`${prefixCls}__input-number`}>
         <InputNumber
           size="small"
-          value={props.value[0]}
+          value={sliderVal[0]}
           onChange={(val) => {
             if (val) {
-              if (val < props.value[1]) {
-                props.onChange([val, props.value[1]]);
+              if (val < sliderVal[1]) {
+                setSliderVal([val, sliderVal[1]]);
               } else {
-                props.onChange([props.value[1], val]);
+                setSliderVal([sliderVal[1], val]);
               }
             } else {
-              props.onChange([0, props.value[1]]);
+              setSliderVal([0, sliderVal[1]]);
             }
           }}
           onBlur={() => {
-            if (!props.value[0]) {
-              props.onChange([0, props.value[1]]);
+            if (!sliderVal[0]) {
+              setSliderVal([0, sliderVal[1]]);
             }
           }}
         />
         <InputNumber
           size="small"
-          value={props.value[1]}
+          value={sliderVal[1]}
           onChange={(val) => {
-            if (val < props.value[0]) {
-              props.onChange([val, props.value[0]]);
+            if (val < sliderVal[0]) {
+              setSliderVal([val, sliderVal[0]]);
             } else {
-              props.onChange([props.value[0], val]);
+              setSliderVal([sliderVal[0], val]);
             }
           }}
           onBlur={() => {
-            if (!props.value[0] && !props.value[1]) {
-              props.onChange([0, 0]);
+            if (!sliderVal[0] && !sliderVal[1]) {
+              setSliderVal([0, 0]);
             }
           }}
         />
