28bae9ae8b50dedb3f40ed72e20859bad4fc155a
yanxiong <oujinhui.ojh@antgroup.com>
2022-08-26 19:27:46 +0800

    Revert "feat: 新增地址搜索组件"
    This reverts commit 9d57a860a66a1d12a105e40ad4d16c0a7e9b4950.

--- src/components/LocationSearch/demos/default.tsx
+++
@@ -1,8 +0,0 @@
-import { LocationSearch, LarkMap } from '@antv/larkmap';
-import React from 'react';
-
-export default () => (
-  <LarkMap mapType="GaodeV1" style={{ height: '300px' }}>
-    <LocationSearch position="topleft" gaodeKey="4892acc9f825e343bcf1e25a56199826" />
-  </LarkMap>
-);
