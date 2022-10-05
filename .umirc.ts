199581bceb3dfb21526c4e5be24a1074c4d316a9
yunji <yunji.me@outlook.com>
2022-10-05 22:13:22 +0800

    chore: 解决样式丢失问题

--- .umirc.ts
+++ .umirc.ts
@@ -48,14 +48,7 @@ export default defineConfig({
     'mapbox-gl': 'window.mapboxgl',
     '@turf/turf': 'window.turf',
   },
-  // 重置浏览器默认样式，后续可加到 @antv/dumi-theme-antv 里
   styles: [
-    `
-    * { margin: 0; padding: 0; box-sizing: border-box; }
-    a { text-decoration: none; }
-    `,
-  ],
-  links: [
     'https://gw.alipayobjects.com/os/lib/antd/4.22.2/dist/antd.css',
     'https://api.tiles.mapbox.com/mapbox-gl-js/v1.13.2/mapbox-gl.css',
   ],
