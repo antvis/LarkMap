60729131c8381490c3cfa360ad11d08d27902d9b
yanxiong <oujinhui.ojh@antgroup.com>
2022-10-12 15:00:04 +0800

    fix: 修复 CustomControl style 部分属性丢失问题

--- src/components/CustomControl/types.ts
+++ src/components/CustomControl/types.ts
@@ -1,17 +1,10 @@
-import type { IControlOption, PositionName } from '@antv/l7';
-import type { CommonProps } from '../../types/common';
+import type { IControlOption } from '@antv/l7';
+import type { CommonProps } from '../../types';
 
 /**
  * 组件类型定义
  */
-
-export interface CustomControlProps extends CommonProps, Partial<IControlOption> {
-  /** 名称 */
-  name?: string;
-  /** 位置，支持 'topright'、'topleft'、'bottomright'、'bottomleft'、'topcenter'、'bottomcenter'、'leftcenter'、'rightcenter'
-   * @default "topleft"
-   */
-  position?: PositionName;
+export interface CustomControlProps extends CommonProps, Omit<Partial<IControlOption>, 'style'> {
   /** 子组件 */
   children?: React.ReactNode;
 }
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,1 +1,2 @@
 export * from './layer-manager';
+export * from './style';
