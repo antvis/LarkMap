6b207c1f56d9d3f19e791d1c3ed292af02d6df76
yanxiong <oujinhui.ojh@antgroup.com>
2022-10-18 20:41:28 +0800

    refactor: 1.迁移 CustomControl 目录结构  2.补充 Control 文档

--- src/components/Control/CustomControl/types.ts
+++ src/components/Control/CustomControl/types.ts
@@ -1,10 +1,10 @@
 import type { IControlOption } from '@antv/l7';
-import type { CommonProps } from '../../types';
+import type { CommonProps } from '../../../types';
 
 /**
  * 组件类型定义
  */
 export interface CustomControlProps extends CommonProps, Omit<Partial<IControlOption>, 'style'> {
-  /** 子组件 */
+  /** 控件容器包含的内容 */
   children?: React.ReactNode;
 }
