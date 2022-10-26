b39cf275a003e742310238a7aa15c3ddb28b9945
yunji <yunji.me@outlook.com>
2022-10-27 01:04:21 +0800

    chore: 迁移 formily 到 @antv/insight-component

--- src/components/LayerAttribute/components/ColorPicker/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { ReactFC } from '@formily/react';
-import { connect } from '@formily/react';
-import type { AntdColorPickerProps } from './AntdColorPicker';
-import AntdColorPicker from './AntdColorPicker';
-
-const ColorPicker: ReactFC<AntdColorPickerProps> = connect(AntdColorPicker);
-
-export default ColorPicker;
