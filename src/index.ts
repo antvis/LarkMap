e7f90222982b60ecfba1b99de272fd377d82c9b0
yunji <yunji.me@outlook.com>
2022-11-02 21:11:51 +0800

    chore: 规范入口文件

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 // organize-imports-ignore
 /**
  * 容器组件
- * */
+ */
 export { LarkMap } from './components/LarkMap';
 export type { LarkMapProps } from './components/LarkMap/types';
 export * from './components/LarkMap/hooks';
@@ -13,42 +13,18 @@ export * from './components/Layers';
 
 /**
  * 控件组件
- * */
-export { CustomControl } from './components/Control/CustomControl';
-export type { CustomControlProps } from './components/Control/CustomControl/types';
-// 兼容修改组件名称，带布局属性的组件加 Control 后缀
-export { ScaleControl, ScaleControl as Scale } from './components/Control/ScaleControl';
-export type { ScaleControlProps, ScaleControlProps as ScaleProps } from './components/Control/ScaleControl/type';
-export { ZoomControl, ZoomControl as Zoom } from './components/Control/ZoomControl';
-export type { ZoomControlProps, ZoomControlProps as ZoomProps } from './components/Control/ZoomControl/types';
-export { FullscreenControl, FullscreenControl as Fullscreen } from './components/Control/FullscreenControl';
-export type {
-  FullscreenControlProps,
-  FullscreenControlProps as FullscreenProps,
-} from './components/Control/FullscreenControl/type';
-export { GeoLocateControl, GeoLocateControl as GeoLocate } from './components/Control/GeoLocateControl';
-export type { GeoLocateControlProps as GeoLocateProps } from './components/Control/GeoLocateControl/type';
-export { ExportImageControl, ExportImageControl as ExportImage } from './components/Control/ExportImageControl';
-export type {
-  ExportImageControlProps,
-  ExportImageControlProps as ExportImageProps,
-} from './components/Control/ExportImageControl/types';
-export { MouseLocationControl, MouseLocationControl as MouseLocation } from './components/Control/MouseLocationControl';
-export type {
-  MouseLocationControlProps,
-  MouseLocationControlProps as MouseLocationProps,
-} from './components/Control/MouseLocationControl/types';
-export { LayerSwitchControl, LayerSwitchControl as LayerSwitch } from './components/Control/LayerSwitchControl';
-export type {
-  LayerSwitchControlProps,
-  LayerSwitchControlProps as LayerSwitchProps,
-} from './components/Control/LayerSwitchControl/types';
-export { MapThemeControl, MapThemeControl as MapTheme } from './components/Control/MapThemeControl';
-export type {
-  MapThemeControlProps,
-  MapThemeControlProps as MapThemeProps,
-} from './components/Control/MapThemeControl/types';
+ */
+export * from './components/Control';
 
+/**
+ * 分析组件
+ */
+export { Template } from './components/Template';
+export type { TemplateProps } from './components/Template/types';
+export { Marker } from './components/Marker';
+export type { MarkerProps } from './components/Marker/types';
+export { Popup } from './components/Popup';
+export type { PopupProps } from './components/Popup/types';
 export { ContextMenu } from './components/ContextMenu';
 export type { ContextMenuItemProps, ContextMenuProps } from './components/ContextMenu/types';
 export { LocationSearch } from './components/LocationSearch';
@@ -57,16 +33,6 @@ export type {
   LocationSearchOption,
   GaodeLocationSearchParams,
 } from './components/LocationSearch/types';
-
-/**
- * 分析组件
- * */
-export { Template } from './components/Template';
-export type { TemplateProps } from './components/Template/types';
-export { Marker } from './components/Marker';
-export type { MarkerProps } from './components/Marker/types';
-export { Popup } from './components/Popup';
-export type { PopupProps } from './components/Popup/types';
 
 /**
  * 图例组件
@@ -95,5 +61,5 @@ export * from './components/LayerAttribute';
 
 /**
  * 版本号
- * */
+ */
 export { default as version } from './version';
