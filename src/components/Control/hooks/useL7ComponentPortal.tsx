e976388a22c7f6e9b0ab4bceb9b3236d9855bff4
yanxiong <oujinhui.ojh@antgroup.com>
2022-11-07 20:45:36 +0800

    fix: 修复 TS 报错

--- src/components/Control/hooks/useL7ComponentPortal.tsx
+++ src/components/Control/hooks/useL7ComponentPortal.tsx
@@ -1,12 +1,17 @@
+import type React from 'react';
+import type { ReactPortal } from 'react';
 import { useRef } from 'react';
-import type { ReactNode } from 'react';
 import { createPortal } from 'react-dom';
 
 /**
  * 将控件参数中的 ReactNode 转成 Element 格式，并返回对应的 DOM 和 Portal，其中 Portal 需要在对应的 React 组件中返回渲染
  * @param reactNode
  */
-export const useL7ComponentPortal = (reactNode?: ReactNode | null) => {
+// @ts-ignore
+export const useL7ComponentPortal: (reactNode?: React.ReactNode | null) => {
+  portal: ReactPortal | null;
+  dom: HTMLElement | undefined;
+} = (reactNode) => {
   const domRef = useRef(document.createElement('span'));
 
   return {
