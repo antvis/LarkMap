30c5b99ee4c5d2c686b4c63701097ff431ceff34
yxh01132861 <yxh01132861@antgroup.com>
2022-11-07 15:42:01 +0800

    fix: 评审问题

--- src/components/LayerAttribute/ChoroplethLayerStyle/constant.ts
+++ src/components/LayerAttribute/ChoroplethLayerStyle/constant.ts
@@ -12,6 +12,6 @@ export const DefaultChoroplethLayerStyle: ChoroplethLayerStyleAttributeValue = {
   lineOpacity: 1,
   label: { style: { fill: 'red', fontSize: 18, textAnchor: 'center', textOffset: [0, 0] } },
   minZoom: 0,
-  maxZoom: 23,
+  maxZoom: 24,
   blend: 'normal',
 };
