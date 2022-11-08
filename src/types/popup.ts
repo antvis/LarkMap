295e77fd6df951b64413a2956422dff71f267a95
syb01094648 <syb01094648@antgroup.com>
2022-11-08 16:01:07 +0800

    fix: 删除无用文件

--- src/types/popup.ts
+++
@@ -1,8 +0,0 @@
-import type { Popup } from '@antv/l7';
-
-export interface IPopupCallback<C extends Popup = Popup> {
-  onOpen?: (control: C) => void;
-  onClose?: (control: C) => void;
-  onShow?: (control: C) => void;
-  onHide?: (control: C) => void;
-}
