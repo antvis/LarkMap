c924d296564f2c2d24feb16d35caccf74b6f1229
yay-01169903 <yay01169903@antgroup.com>
2022-11-14 10:08:59 +0800

    fix: 洛杉矶房屋删除点

--- docs/example/losAngelesHomes/index.tsx
+++ docs/example/losAngelesHomes/index.tsx
@@ -1,11 +1,4 @@
-import {
-  ChoroplethLayer,
-  ChoroplethLayerProps,
-  LarkMap,
-  LarkMapProps,
-  PointLayer,
-  PointLayerProps,
-} from '@antv/larkmap';
+import { ChoroplethLayer, ChoroplethLayerProps, LarkMap, LarkMapProps } from '@antv/larkmap';
 import { polygon } from '@turf/turf';
 import { cellToBoundary, latLngToCell } from 'h3-js';
 import React, { useEffect, useState } from 'react';
@@ -29,29 +22,12 @@ export default () => {
     },
   };
 
-  const layerOptions = {
-    autoFit: true,
-    shape: 'circle',
-    size: 4,
-    opacity: 0.4,
-    blend: 'normal',
-    color: {
-      field: 'roll_totalvalue',
-      value: [
-        'rgb(102,37,6)',
-        'rgb(153,52,4)',
-        'rgb(204,76,2)',
-        'rgb(236,112,20)',
-        'rgb(254,153,41)',
-        'rgb(254,196,79)',
-        'rgb(254,227,145)',
-      ],
-    },
-  };
   useEffect(() => {
     fetch('https://gw.alipayobjects.com/os/bmw-prod/2dfb73f7-df5a-46e5-9037-846c481d9e45.json')
       .then((res) => res.json())
-      .then((data) => setPointData({ ...PonitData, data }));
+      .then((data) => {
+        setPointData({ ...PonitData, data: data.slice(0, 100000) });
+      });
   }, []);
 
   useEffect(() => {
@@ -76,8 +52,18 @@ export default () => {
 
   const layerOption = {
     autoFit: true,
-    fillColor: '#377eb8',
-    opacity: 0.4,
+    fillColor: '#5B8FF9',
+    // field: 'roll_totalvalue',
+    //   value: [
+    //     'rgb(102,37,6)',
+    //     'rgb(153,52,4)',
+    //     'rgb(204,76,2)',
+    //     'rgb(236,112,20)',
+    //     'rgb(254,153,41)',
+    //     'rgb(254,196,79)',
+    //     'rgb(254,227,145)',
+    //   ],
+    opacity: 0.6,
     strokeColor: '#fff',
     lineWidth: 1,
     state: {
@@ -88,7 +74,6 @@ export default () => {
 
   return (
     <LarkMap {...(config as LarkMapProps)} style={{ height: '60vh' }}>
-      <PointLayer {...(layerOptions as unknown as PointLayerProps)} source={PonitData} />
       <ChoroplethLayer
         {...(layerOption as unknown as ChoroplethLayerProps)}
         source={{ data: polygonData, parser: { type: 'geojson' } }}
