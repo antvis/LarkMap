c228ddc11746a12f2c4d1b06272f404aa29cccf2
yanxiong <oujinhui.ojh@antgroup.com>
2022-11-19 21:30:30 +0800

    docs: 修复 Marker Demo 偏移问题

--- src/components/Marker/demos/custom.tsx
+++ src/components/Marker/demos/custom.tsx
@@ -1,15 +1,20 @@
 import type { LarkMapProps } from '@antv/larkmap';
 import { LarkMap, Marker } from '@antv/larkmap';
+import { message } from 'antd';
 import React from 'react';
 
 const larkmapOptions: LarkMapProps = {
   mapType: 'Gaode',
-  mapOptions: { center: [120.104735, 30.261121], zoom: 10 },
+  mapOptions: { center: [120.210792, 30.246026], zoom: 10 },
 };
 
 export default () => (
   <LarkMap {...larkmapOptions} style={{ height: '300px' }}>
-    <Marker lngLat={{ lng: 120.210792, lat: 30.246026 }}>
+    <Marker
+      lngLat={{ lng: 120.210792, lat: 30.246026 }}
+      anchor="center"
+      onClick={() => message.success('Marker clicked!')}
+    >
       <div style={{ padding: 8, backgroundColor: 'pink' }}>杭州</div>
     </Marker>
   </LarkMap>
