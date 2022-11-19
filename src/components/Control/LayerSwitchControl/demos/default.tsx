c9b2f735a4e126099dfda6ec4e1d65c52610b5b2
yanxiong <oujinhui.ojh@antgroup.com>
2022-11-19 23:22:27 +0800

    docs: 完善 LayerSwitchControl 示例

--- src/components/Control/LayerSwitchControl/demos/default.tsx
+++ src/components/Control/LayerSwitchControl/demos/default.tsx
@@ -1,65 +1,52 @@
-import { LarkMap, LayerSwitchControl, LineLayer, PolygonLayer } from '@antv/larkmap';
-import React from 'react';
+import type { PolygonLayerProps } from '@antv/larkmap';
+import { LarkMap, LayerSwitchControl, PolygonLayer } from '@antv/larkmap';
+import React, { useEffect, useState } from 'react';
+
+const polygonLayerOptions: Omit<PolygonLayerProps, 'source'> = {
+  autoFit: true,
+  shape: 'fill',
+  color: {
+    field: 'adcode',
+    value: ['#0f9960', '#33a02c', '#477eb8'],
+  },
+  state: {
+    active: true,
+  },
+  style: {
+    opacity: 0.6,
+  },
+};
+
+const lineLayerOptions: Omit<PolygonLayerProps, 'source'> = {
+  shape: 'line',
+  size: 2,
+  color: '#595959',
+  state: {
+    active: false,
+  },
+};
 
 export default () => {
+  const [source, setSource] = useState({
+    data: { type: 'FeatureCollection', features: [] },
+    parser: { type: 'geojson' },
+  });
+
+  useEffect(() => {
+    fetch('https://gw.alipayobjects.com/os/antfincdn/Y8eGLb9j9v/hangzhou-district.json')
+      .then((response) => response.json())
+      .then((data: any) => {
+        setSource((prevState) => ({ ...prevState, data }));
+      });
+  }, []);
+
   return (
     <LarkMap mapType="Gaode" style={{ height: '400px' }}>
       <LayerSwitchControl layers={['边框图层', '填充图层']} />
 
-      <LineLayer
-        name="边框图层"
-        size={2}
-        color={'rgb(255,0,0)'}
-        source={{
-          data: {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'LineString',
-                  coordinates: [
-                    [120.12039184570312, 30.181934730780572],
-                    [120.25634765624999, 30.181934730780572],
-                    [120.25634765624999, 30.274486436999464],
-                    [120.12039184570312, 30.274486436999464],
-                    [120.12039184570312, 30.181934730780572],
-                  ],
-                },
-              },
-            ],
-          },
-        }}
-      />
+      <PolygonLayer name="填充图层" {...polygonLayerOptions} source={source} />
 
-      <PolygonLayer
-        name="填充图层"
-        color={'rgba(255,0,0,0.5)'}
-        source={{
-          data: {
-            type: 'FeatureCollection',
-            features: [
-              {
-                type: 'Feature',
-                properties: {},
-                geometry: {
-                  type: 'Polygon',
-                  coordinates: [
-                    [
-                      [120.12039184570312, 30.181934730780572],
-                      [120.25634765624999, 30.181934730780572],
-                      [120.25634765624999, 30.274486436999464],
-                      [120.12039184570312, 30.274486436999464],
-                      [120.12039184570312, 30.181934730780572],
-                    ],
-                  ],
-                },
-              },
-            ],
-          },
-        }}
-      />
+      <PolygonLayer name="边框图层" {...lineLayerOptions} source={source} />
     </LarkMap>
   );
 };
