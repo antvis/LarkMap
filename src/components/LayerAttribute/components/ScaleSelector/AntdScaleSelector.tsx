4067fc56964ae8860eae3d72c7b3fbdb89406512
yxh01132861 <yxh01132861@antgroup.com>
2023-01-03 18:33:02 +0800

    fix: 代码问题处理

--- src/components/LayerAttribute/components/ScaleSelector/AntdScaleSelector.tsx
+++ src/components/LayerAttribute/components/ScaleSelector/AntdScaleSelector.tsx
@@ -10,20 +10,20 @@ interface OptionType extends DefaultOptionType {
 export type AntdScaleSelectorProps = SelectProps<string, OptionType> & { type: 'string' | 'number' };
 
 const AntdScaleSelector = (props: AntdScaleSelectorProps) => {
-  const { type, options, ...others } = props;
+  const { type, options, ...othersProps } = props;
 
   const selectOptions = useMemo(() => {
     return options.filter((item) => item.type === type);
   }, [type, options]);
 
   useEffect(() => {
-    if (!others.value || selectOptions.findIndex((item) => item.value === others.value) === -1) {
-      others?.onChange(selectOptions[0]?.value, selectOptions);
+    if (!othersProps.value || selectOptions.findIndex((item) => item.value === othersProps.value) === -1) {
+      othersProps?.onChange(selectOptions[0]?.value, selectOptions);
     }
   }, [selectOptions]);
 
   return (
-    <Select {...others}>
+    <Select {...othersProps}>
       {selectOptions?.map((item, index) => {
         return (
           <Select.Option value={item.value} key={index}>
