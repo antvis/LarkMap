02b3523e4d84a5fb2d7d66b8d72bafcff7d77201
suyubin <84109842+websybin@users.noreply.github.com>
2023-02-20 10:31:17 +0800

    feat: 下载行政区域页面demo (#136)
    * fix: 下载行政区域页面demo
    
    * fix: 下载行政区域页面demo
    
    * fix: 解决评论问题
    
    * fix: 部分下钻逻辑
    
    * fix: 数据源l7下钻功能初步完成
    
    * fix: 上卷下钻逻辑全部完成
    
    * fix: 部分功能完成
    
    * fix: 提交less文件
    
    * fix: 下载行政区域页面demo
    
    * fix: 删除无用代码
    
    * fix: 接口导出
    
    * fix: 尝试解决ci问题
    
    * fix: 提交代码
    
    * fix: 解决ci报错问题
    
    * fix: 调整ci问题
    
    * fix: 调整linmintsize大小
    
    * fix: 添加LayerPopup
    
    * fix: 修改初始展示数据
    
    * fix: ci报错问题
    
    * fix: 解决问题
    
    * fix: 修改文件位置
    
    * fix: 提交less文件
    
    * fix: 修改配置文件
    
    * fix: 接口请求处理
    
    * fix: 九段线和国界线数据合并
    
    * fix: 添加loading
    
    * fix: 文件下载动态获取
    
    * fix: 调整样式
    
    * fix: 添加功能
    
    * fix: 支持高级设置数据精度下载
    
    * fix: 完成点击图层下载
    
    * fix: 调整样式
    
    * fix: 完成点击图层下载
    
    * fix: 完成点击图层下载
    
    * fix: 修复问题
    
    * fix: 调整样式
    
    * fix: 调整样式
    
    * fix: 调整样式

---
+++ docs/example/regionDownload/data/dataSource.ts
@@ -0,0 +1,153 @@
+import { simplify } from '@turf/turf';
+import geobuf from 'geobuf';
+import Pbf from 'pbf';
+import { getFetch } from '../util';
+
+export class DataSource {
+  public DataVSource = { type: 'FeatureCollection', features: [] };
+  public line: any;
+  public country: any;
+  public province: any;
+  public city: any;
+  public district: any;
+  constructor() {
+    this.init();
+  }
+
+  L7Geojson = (data) => {
+    return geobuf.decode(new Pbf(data));
+  };
+
+  gitCountryData = async (accuracy: number = 0.005) => {
+    const options = { tolerance: accuracy, highQuality: false };
+    const L7CountryData = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_guojie'));
+    const L7CountryDataJson = await L7CountryData.arrayBuffer();
+    this.country = await simplify(this.L7Geojson(L7CountryDataJson), options);
+    const data = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_jiuduanxian'));
+    const L7LineDataJson = await data.arrayBuffer();
+    this.line = await simplify(this.L7Geojson(L7LineDataJson), options);
+    this.country = { type: 'FeatureCollection', features: [...this.country.features, ...this.line.features] };
+    return await this.country;
+  };
+
+  gitData = async (accuracy: number = 0.005, cityLevel: 'country' | 'province' | 'city' | 'district') => {
+    const options = { tolerance: accuracy, highQuality: false };
+    const L7CountryData = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_guojie'));
+    const L7CountryDataJson = await L7CountryData.arrayBuffer();
+    const data = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_jiuduanxian'));
+    const L7LineDataJson = await data.arrayBuffer();
+    const CountryDataJson = await simplify(this.L7Geojson(L7CountryDataJson), options);
+    const dataJson = await simplify(this.L7Geojson(L7LineDataJson), options);
+    const newCountryDataJson = {
+      type: 'FeatureCollection',
+      features: [...CountryDataJson.features, ...dataJson.features],
+    };
+    const L7ProvinceData = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_sheng'));
+    const L7ProvinceDataJson = await L7ProvinceData.arrayBuffer();
+    const provinceDataJson = await simplify(this.L7Geojson(L7ProvinceDataJson), options);
+    const L7CityData = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_shi'));
+    const L7CityDataJson = await L7CityData.arrayBuffer();
+    const cityDataJson = await simplify(this.L7Geojson(L7CityDataJson), options);
+    const L7DistrictData = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', '2023_xian'));
+    const L7DistrictDataJson = await L7DistrictData.arrayBuffer();
+    const districtDataJson = await simplify(this.L7Geojson(L7DistrictDataJson), options);
+    return {
+      country: newCountryDataJson,
+      province: provinceDataJson,
+      city: cityDataJson,
+      district: districtDataJson,
+    }[cityLevel];
+  };
+
+  gitCityData = async (accuracy: number = 0.005) => {
+    const options = { tolerance: accuracy, highQuality: false };
+  };
+
+  init = async () => {
+    const dataVData = await fetch(getFetch('dataV', 'areas_v3', '100000_full'));
+    this.DataVSource = await dataVData.json();
+  };
+
+  gitL7GeoJson = async (city: string, cityLevel: string) => {
+    const options = { tolerance: 0.005, highQuality: false };
+
+    const L7CountryData = await fetch(getFetch('L7', 'xinzhengqu@1.0.0', city));
+    const L7CountryDataJson = await L7CountryData.arrayBuffer();
+    this[cityLevel] = await simplify(this.L7Geojson(L7CountryDataJson), options);
+    return await simplify(this.L7Geojson(L7CountryDataJson), options);
+  };
+
+  gitFetchData = async (areaLevel: 'country' | 'province' | 'city' | 'district') => {
+    if (areaLevel === 'country') {
+      if (this.country) {
+        return this.country;
+      } else {
+        return await this.gitL7GeoJson('2023_guojie', 'country');
+      }
+    }
+    if (areaLevel === 'province') {
+      if (this.province) {
+        return this.province;
+      } else {
+        return await this.gitL7GeoJson('2023_sheng', 'province');
+      }
+    }
+    if (areaLevel === 'city') {
+      if (this.city) {
+        return this.city;
+      } else {
+        return await this.gitL7GeoJson('2023_shi', 'city');
+      }
+    }
+    if (areaLevel === 'district') {
+      if (this.district) {
+        return this.district;
+      } else {
+        return await this.gitL7GeoJson('2023_xian', 'district');
+      }
+    }
+  };
+
+  getCityData = async (
+    dataType: 'country' | 'province' | 'city' | 'district',
+    code: number,
+    codeIndex: string,
+    drillLevel: string,
+    level?: 'country' | 'province',
+  ) => {
+    if (level === 'country') {
+      const data = await this.gitFetchData('province');
+      const dataJson = { type: 'FeatureCollection', features: data.features };
+      return {
+        geoJson: dataJson,
+        code: 100000,
+        areaLevel: 'province',
+        GID_1: undefined,
+        GID_2: undefined,
+      };
+    } else if (level === 'province') {
+      const data = await this.gitFetchData('country');
+      const dataJson = { type: 'FeatureCollection', features: data.features };
+      return {
+        geoJson: dataJson,
+        code: 100000,
+        areaLevel: 'country',
+        GID_1: undefined,
+        GID_2: undefined,
+      };
+    } else {
+      const data = await this.gitFetchData(dataType);
+      const filterData = data.features.filter((item) => {
+        return item.properties[codeIndex] === code;
+      });
+      const dataJson = { type: 'FeatureCollection', features: codeIndex === '' ? data.features : filterData };
+      return {
+        geoJson: dataJson,
+        code,
+        areaLevel: drillLevel,
+        GID_1: dataJson?.features[0]?.properties?.GID_1,
+        GID_2: dataJson?.features[0]?.properties?.GID_2,
+      };
+    }
+  };
+}
