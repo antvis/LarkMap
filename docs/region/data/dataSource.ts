3d303008b35e31a848cb51f87b129ff46970e6ee
websybin <2662826571@qq.com>
2023-02-21 14:40:32 +0800

    fix: 调整代码

--- docs/region/data/dataSource.ts
+++ docs/region/data/dataSource.ts
@@ -1,21 +1,23 @@
-import { getFetch } from '../util';
-
 export class DataVSource {
   public DataVSource = { type: 'FeatureCollection', features: [] };
   constructor() {
     this.init();
   }
 
+  getFetch = (version: string, code: string | number) => {
+    return `https://geo.datav.aliyun.com/${version}/bound/${code}.json`;
+  };
+
   init = async () => {
-    const dataVData = await fetch(getFetch('dataV', 'areas_v3', '100000_full'));
+    const dataVData = await fetch(this.getFetch('areas_v3', '100000_full'));
     this.DataVSource = await dataVData.json();
   };
 
   gitDataVData = async (code: number, full?: string) => {
     if (full) {
-      return fetch(getFetch('dataV', 'areas_v3', `${code}_full`));
+      return fetch(this.getFetch('areas_v3', `${code}_full`));
     } else {
-      return fetch(getFetch('dataV', 'areas_v3', `${code}`));
+      return fetch(this.getFetch('areas_v3', `${code}`));
     }
   };
 }
--- docs/region/util.ts
+++ docs/region/util.ts
@@ -5,21 +5,6 @@ import type { DataVSource } from './data/dataSource';
 
 /**
  *
- * @param fetchType 数据类型
- * @param version 版本号
- * @param code 数据code
- * @returns
- */
-export const getFetch = (fetchType: 'dataV' | 'L7', version: string, code: string | number) => {
-  if (fetchType === 'dataV') {
-    return `https://geo.datav.aliyun.com/${version}/bound/${code}.json`;
-  } else {
-    return `https://unpkg.com/${version}/data/${code}.pbf`;
-  }
-};
-
-/**
- *
  * @param sourceValue 数据源类型
  * @param code 对应编码
  * @param areaLevel 城市等级
