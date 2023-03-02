55557d6a583aaa0c8bc3bfef7d34a7e3dfbe03bc
lvisei <yunji.me@outlook.com>
2023-03-02 19:31:43 +0800

    fix: 修复 umd 打包依赖问题 (#156)

--- docs/region/data/index.ts
+++
@@ -1,9 +0,0 @@
-// export { DataVSource } from './dataSource';
-import { DataVSource } from './datavSource';
-import { L7Source } from './l7Source';
-export { default as BaseSource } from './BaseDataSource';
-export const DataSourceMap = {
-  DataVSource,
-  L7Source,
-};
-export type SourceType = keyof typeof DataSourceMap;
