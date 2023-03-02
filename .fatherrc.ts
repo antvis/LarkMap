b107f40d91bdacaf0748e38efba908d9d69bf7ff
yunji <yunji.me@outlook.com>
2023-03-02 19:19:04 +0800

    fix: 修复 umd 打包依赖问题

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -14,7 +14,7 @@ export default defineConfig({
       react: 'React',
       'react-dom': 'ReactDOM',
       '@antv/l7': 'L7',
-      '@antv/l7-draw': 'L7.Draw',
+      '@antv/l7-draw': { root: ['L7', 'Draw'], commonjs2: '@antv/l7-draw', commonjs: '@antv/l7-draw' },
     },
     chainWebpack(memo) {
       memo
