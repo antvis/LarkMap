7e95207960a15195f15ad2ba087ce05f5649e490
websybin <2662826571@qq.com>
2023-06-15 09:40:07 +0800

    fix: 删除deom多余配置

--- src/components/Layers/CompositeLayers/FlowLayer/demos/default.tsx
+++ src/components/Layers/CompositeLayers/FlowLayer/demos/default.tsx
@@ -1,42 +1,5 @@
-import type { FlowLayerProps } from '@antv/larkmap';
 import { FlowLayer, LarkMap, useScene } from '@antv/larkmap';
 import React, { useEffect, useState } from 'react';
-
-const trafficFlowLayerOptions: Omit<FlowLayerProps, 'source'> = {
-  // 客流点聚合类型
-  clusterType: 'HCA',
-  // zoom 计算步长
-  clusterZoomStep: 1,
-  // 聚合点像素尺寸
-  clusterNodeSize: 64,
-  // 聚合半径
-  clusterRadius: 40,
-  // 聚合力度
-  clusterExtent: 512,
-  // 最大展示的客流线条数
-  maxTopFlowNum: 5000,
-  color: '#fff',
-  zoom: 12,
-  radius: {
-    field: 'weight',
-    value: [1, 16],
-  },
-  lineColor: {
-    field: 'weight',
-    value: ['orange', 'red'],
-  },
-  lineSize: {
-    field: 'weight',
-    value: [1, 16],
-  },
-  // 是否启用根据权重映射半透明值
-  fadeOpacityEnabled: true,
-  // 半透明的权重
-  fadeOpacityAmount: 0,
-  state: {
-    select: { strokeColor: 'pink', lineWidth: 1.5, lineOpacity: 0.8 },
-  },
-};
 
 const parser = {
   type: 'json',
@@ -57,7 +20,7 @@ const FlowLayers = ({ sourceData }: { sourceData: Record<string, any> }) => {
     }
   }, [scene, sourceData]);
 
-  return <FlowLayer {...trafficFlowLayerOptions} source={{ data: sourceData, parser }} />;
+  return <FlowLayer source={{ data: sourceData, parser }} />;
 };
 
 export default () => {
