827dff821978a857318e06aece319ba200325a17
websybin <2662826571@qq.com>
2023-06-15 13:29:05 +0800

    fix: demo修改

--- src/components/Layers/CompositeLayers/FlowLayer/demos/default.tsx
+++ src/components/Layers/CompositeLayers/FlowLayer/demos/default.tsx
@@ -1,36 +1,24 @@
-import { FlowLayer, LarkMap, useScene } from '@antv/larkmap';
+import { FlowLayer, LarkMap } from '@antv/larkmap';
 import React, { useEffect, useState } from 'react';
 
-const parser = {
-  type: 'json',
-  x: 'f_lon',
-  y: 'f_lat',
-  x1: 't_lon',
-  y1: 't_lat',
-  weight: 'weight',
-};
-
-const FlowLayers = ({ sourceData }: { sourceData: Record<string, any> }) => {
-  const scene = useScene();
-
-  useEffect(() => {
-    const { f_lon, f_lat } = sourceData[sourceData.length / 2];
-    if (scene) {
-      scene.setCenter([+f_lon, +f_lat]);
-    }
-  }, [scene, sourceData]);
-
-  return <FlowLayer source={{ data: sourceData, parser }} />;
-};
-
 export default () => {
-  const [sourceData, setSourceData] = useState([]);
+  const [source, setSource] = useState({
+    data: [],
+    parser: {
+      type: 'json',
+      x: 'f_lon',
+      y: 'f_lat',
+      x1: 't_lon',
+      y1: 't_lat',
+      weight: 'weight',
+    },
+  });
 
   useEffect(() => {
     fetch('https://gw.alipayobjects.com/os/bmw-prod/f4f3e99a-1d6c-4ab0-b08f-ec730c576b62.json')
       .then((response) => response.json())
       .then((data: any) => {
-        setSourceData(data);
+        setSource((prevState) => ({ ...prevState, data }));
       });
   }, []);
 
@@ -39,10 +27,11 @@ export default () => {
       mapType="Gaode"
       mapOptions={{
         style: 'dark',
+        center: [121.29, 31.14],
       }}
       style={{ height: '50vh' }}
     >
-      {sourceData.length !== 0 && <FlowLayers sourceData={sourceData} />}
+      {source.data.length !== 0 && <FlowLayer source={source} />}
     </LarkMap>
   );
 };
