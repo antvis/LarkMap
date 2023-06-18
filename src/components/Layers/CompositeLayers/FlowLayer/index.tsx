69acc1628830e585cdd5f30d01b85d1989d8e690
yanxiong <oujinhui.ojh@antgroup.com>
2023-06-18 15:25:40 +0800

    fix: 还原客流图层的鼠标事件

--- src/components/Layers/CompositeLayers/FlowLayer/index.tsx
+++ src/components/Layers/CompositeLayers/FlowLayer/index.tsx
@@ -3,33 +3,11 @@ import { forwardRef, memo, useImperativeHandle } from 'react';
 import { useCreateLayer, useLayerEvent } from '../../hooks';
 import type { FlowLayerProps } from './types';
 
-//TODD 图层没有鼠标事件
-const FlowLayerEventMap = Object.assign(
-  {
-    onRemove: 'remove',
-    onShow: 'show',
-    onHide: 'hide',
-    onDataUpdate: 'dataUpdate',
-    onLegend: 'legend',
-    onLegendColor: 'legend:color',
-    onLegendSize: 'legend:size',
-
-    // 点击事件
-    onClick: 'click',
-    onUnClick: 'unclick',
-    onDblClick: 'dblclick',
-    onUndblclick: 'undblclick',
-    onContextMenu: 'contextmenu',
-    onUnContextMenu: 'uncontextmenu',
-  },
-  {},
-);
-
 export const FlowLayer = memo(
   forwardRef<L7FlowLayer, FlowLayerProps>(function TrafficFlowLayer(props, ref) {
     const layerRef = useCreateLayer<L7FlowLayer, FlowLayerProps>(L7FlowLayer, props);
     //@ts-ignore
-    useLayerEvent(layerRef.current, props, FlowLayerEventMap);
+    useLayerEvent(layerRef.current, props);
     useImperativeHandle(ref, () => layerRef.current);
 
     return null;
