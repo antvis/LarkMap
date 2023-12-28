e00ff06704753cd6387e40d8cebe810a40400197
yanxiong <oujinhui.ojh@antgroup.com>
2023-12-28 11:53:22 +0800

    feat: 新增 LayerSwitch 支持传递 layer Id

--- src/components/Control/LayerSwitchControl/types.ts
+++ src/components/Control/LayerSwitchControl/types.ts
@@ -1,10 +1,17 @@
 import type { ILayerSwitchOption, LayerSwitch } from '@antv/l7';
 import type { CSSProperties, ReactNode } from 'react';
-import type { IPopperControlCallback } from '../../../types';
+import type { IPopperControlCallback, Layer } from '../../../types';
+
+export type LayerSwitchItem = {
+  layer: Layer | string;
+  name?: string;
+  img?: string;
+};
 
 export interface LayerSwitchControlProps
-  extends Omit<Partial<ILayerSwitchOption>, 'style' | 'btnIcon'>,
+  extends Omit<Partial<ILayerSwitchOption>, 'style' | 'btnIcon' | 'layers'>,
     IPopperControlCallback<LayerSwitch> {
+  layers: (Layer | string | LayerSwitchItem)[];
   style?: CSSProperties;
   btnIcon?: ReactNode;
   onSelectChange?: (value: string[]) => void;
