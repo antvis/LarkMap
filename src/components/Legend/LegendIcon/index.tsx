52fc2f83e87a43a8908c9797cb5cc1ce5fe1bff4
hsp-sz <HSimple1999@163.com>
2024-04-10 15:06:33 +0800

    feat: 自定义icons

--- src/components/Legend/LegendIcon/index.tsx
+++ src/components/Legend/LegendIcon/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
 import classnames from 'classnames';
-import type { LegendIconProps } from './types';
+import React from 'react';
 import './index.less';
+import type { LegendIconProps, LegendIcons } from './types';
 
 export const CLS_PREFIX = 'larkmap-legend-icon';
 
 export const LegendIcon = (props: LegendIconProps) => {
   const { labels, icons, className: cls, style } = props;
+
+  const renderIcon = (icon: LegendIcons) => {
+    if (React.isValidElement(icon)) {
+      return icon;
+    } else if (typeof icon === 'string') {
+      return <img src={icon} />;
+    }
+  };
+
   return (
     <div className={classnames(`${CLS_PREFIX}`, cls)} style={style}>
       {labels.map((item, index) => (
         <div key={item} className={`${CLS_PREFIX}__content`}>
-          <img src={icons[index]} className={`${CLS_PREFIX}__content__icon`} />
+          {renderIcon(icons[index])}
           <div>{item}</div>
         </div>
       ))}
--- src/components/Legend/LegendIcon/types.ts
+++ src/components/Legend/LegendIcon/types.ts
@@ -1,8 +1,10 @@
 import type { CommonProps } from '../../../types/common';
 
+export type LegendIcons = string | React.ReactElement;
+
 export interface LegendIconProps extends CommonProps {
   /** 图例项名称 */
   labels: string[];
   /** 图例项图标 */
-  icons: string[];
+  icons: LegendIcons[];
 }
