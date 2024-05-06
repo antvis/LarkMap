a05834f4efe8df9ed1574a18a1c4252ed7639374
zhaoqianci <zqc01094647@antgroup.com>
2024-05-06 10:40:23 +0800

    fix: 修改 LocationSearch 高德 key 值

--- src/components/LocationSearch/constant.ts
+++ src/components/LocationSearch/constant.ts
@@ -2,9 +2,3 @@
 export const CLS_PREFIX = 'larkmap-location-search';
 
 export const GAO_DE_API_URL = 'https://restapi.amap.com/v3/assistant/inputtips';
-
-export const AMAP_AVAILABLE_KEY_LIST = [
-  '20a5ed5ecb2756a0361e83e794b4aa0a',
-  'fdd01f9a9dd6846239b9523d7923339b',
-  '053b838db2f7b6a871b6ec5dbf62ab62',
-];
